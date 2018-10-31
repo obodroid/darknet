99d81d9937daeaf469aecb872e9093475be623e0
Jiradej Vatanutanon <jiradej_cu@hotmail.com>
2018-10-31 16:00:12 +0700

    remove duplicate code

--- python/websocket-server.py
+++ python/websocket-server.py
@@ -131,11 +131,10 @@ def main(reactor):
     log.startLogging(sys.stdout)
     factory = WebSocketServerFactory()
     factory.protocol = DarknetServerProtocol
-    reactor.listenTCP(args.port, factory)
-    reactor.run()
-    # factory.protocol = DarknetServerProtocol
     # ctx_factory = DefaultOpenSSLContextFactory(tls_key, tls_crt)
     # reactor.listenSSL(args.port, factory, ctx_factory)
+    reactor.listenTCP(args.port, factory)
+    reactor.run()
     return Deferred()
 
 
