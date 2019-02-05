a75bf0626d8593442b16caf6ae74d888e2a8c949
Lertlove <lertluck.l@hiveground.com>
2019-02-05 09:29:24 +0700

    increase auto ping timeout

--- python/websocket-server.py
+++ python/websocket-server.py
@@ -184,7 +184,7 @@ def main(reactor):
     observer = log.startLogging(sys.stdout)
     observer.timeFormat = "%Y-%m-%d %T.%f"
     factory = WebSocketServerFactory()
-    factory.setProtocolOptions(autoPingInterval=1, autoPingTimeout=5, autoFragmentSize=1000000)
+    factory.setProtocolOptions(autoPingInterval=1, autoPingTimeout=10, autoFragmentSize=1000000)
     factory.protocol = DarknetServerProtocol
     # ctx_factory = DefaultOpenSSLContextFactory(tls_key, tls_crt)
     # reactor.listenSSL(args.port, factory, ctx_factory)
